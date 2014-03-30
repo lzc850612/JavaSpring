0d7c274541ebf0af7fa9b375787070ea46490f43
lzc850612 <lzc850612@gmail.com>
2014-03-30 18:34:06 +0800

    added code of JavaSpring project

---
+++ src/main/java/com/ted/webapp/controller/ResetPasswordController.java
@@ -0,0 +1,32 @@
+package com.ted.webapp.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.appfuse.model.User;
+import com.ted.webapp.util.RequestUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+@RequestMapping("/resetpassword*")
+public class ResetPasswordController extends BaseFormController {
+
+    public ResetPasswordController() {
+        setCancelView("redirect:login");
+        setSuccessView("redirect:home");
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handleRequest()
+    {
+        return new ModelAndView();
+    }
+}
---
+++ src/main/webapp/requestRecoveryToken.jsp
@@ -0,0 +1,10 @@
+<%@ include file="/common/taglibs.jsp"%>
+
+<head>
+    <title><fmt:message key="home.title"/></title>
+    <meta name="menu" content="Home"/>
+</head>
+<body class="home">
+
+<h2><fmt:message key="home.heading"/></h2>
+<p><fmt:message key="home.message"/></p>
